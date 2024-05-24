75cfe9fd672c4e7f5f10e6486a6dc016ac875ff8
Jesse Gerbrandt <jgerbrandt@live.com>
2024-05-24 16:04:15 -0600

    preprocess update

--- bio-rocket/svelte.config.js
+++ bio-rocket/svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+	preprocess: preprocess({postcss: true}),
 	
 	vitePlugin: {
 		inspector: true,
