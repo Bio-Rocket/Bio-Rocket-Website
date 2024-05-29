03e77d6bb8e95b93616e3ae28ccdb1a3c97899aa
Jesse Gerbrandt <jgerbrandt@live.com>
2024-05-29 14:28:49 -0600

    adding about us page

--- bio-rocket/src/routes/about-us/+page.svelte
+++ bio-rocket/src/routes/about-us/+page.svelte
@@ -1,3 +1,129 @@
-<div class="absolute top-40 left-1/2 transform -translate-x-1/2 font-bold text-4xl">
-    *Coming Soon*
+<script lang="ts">
+    import Jesse from "$lib/assets/jesse.png";
+    import Jayden from "$lib/assets/jayden.png";
+    import Lukas from "$lib/assets/lukas.png";
+    import Jerry from "$lib/assets/jerry.png";
+    import Rimoon from "$lib/assets/rimoon.png";
+    import Wade from "$lib/assets/wade.png";
+    
+</script>
+
+<div class="container mx-auto flex flex-col justify-center items-center overflow-auto">
+	<h1 class="text-center text-5xl mt-8 mb-8 text-primary-500">Meet the Visionaries</h1>
+    <div class="w-full mb-8 text-token grid grid-cols-3 md:grid-cols-3 gap-4">
+		<div class="card bg-initial card-hover overflow-hidden">
+            <div class="mt-4 flex justify-center items-center">
+                <img src={Jayden} alt="Jesse" class="h-64 object-cover object-contain">
+            </div>
+			<header>
+				<h3 class="h3 mt-4 flex justify-center items-center" data-toc-ignore><strong>Jayden Sorensen                </strong></h3>
+                <h4 class="h4 flex justify-center items-center text-primary-500" data-toc-ignore>Controls Lead</h4>
+			</header>
+			<div class="p-4 space-y-4">
+				<article>
+					<p class="text-center">
+						<!-- cspell:disable -->
+						After 3 years with SOAR and 24 months of aerospace and research internship experience, Jayden is ready to design, integrate, and test systems for data acquisition and analysis, communication, controls, and much more.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="opacity-50" />
+		</div>
+        <div class="card bg-initial card-hover overflow-hidden">
+            <div class="mt-4 flex justify-center items-center">
+                <img src={Jerry} alt="Jesse" class="h-64 object-cover object-contain">
+            </div>
+			<header>
+				<h3 class="h3 mt-4 flex justify-center items-center" data-toc-ignore><strong>Jerry Wang</strong></h3>
+                <h4 class="h4 flex justify-center items-center text-primary-500" data-toc-ignore>Sustainability Lead</h4>
+			</header>
+			<div class="p-4 space-y-4">
+				<article>
+					<p class="text-center">
+						<!-- cspell:disable -->
+						Jerry has been working on rocket propulsion systems with SOAR for the past 4 years, and has 12 months of internship experience in the aviation industry. With his major in biomedical engineering, he is ready to push the envelope of sustainable fuels. 
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="opacity-50" />
+		</div>
+        <div class="card bg-initial card-hover overflow-hidden">
+            <div class="mt-4 flex justify-center items-center">
+                <img src={Rimoon} alt="Jesse" class="h-64 object-cover object-contain">
+            </div>
+			<header>
+				<h3 class="h3 mt-4 flex justify-center items-center" data-toc-ignore><strong>Rimoon Koryal                </strong></h3>
+                <h4 class="h4 flex justify-center items-center text-primary-500" data-toc-ignore>Test Lead</h4>
+			</header>
+			<div class="p-4 space-y-4">
+				<article>
+					<p class="text-center">
+						<!-- cspell:disable -->
+						With a 16-month engineering internship under his belt, Rimoon has experienced everything from safety regulation revision, embedded programming, to modelling and drafting parts for impressively large assemblies geared toward an in-house production line.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="opacity-50" />
+		</div>
+        <div class="card bg-initial card-hover overflow-hidden">
+            <div class="mt-4 flex justify-center items-center">
+                <img src={Jesse} alt="Jesse" class="h-64 object-cover object-contain">
+            </div>
+			<header>
+				<h3 class="h3 mt-4 flex justify-center items-center" data-toc-ignore><strong>Jesse Gerbrandt</strong></h3>
+                <h4 class="h4 flex justify-center items-center text-primary-500" data-toc-ignore>Software Lead</h4>
+			</header>
+			<div class="p-4 space-y-4">
+				<article>
+					<p class="text-center">
+						<!-- cspell:disable -->
+						With 2 years of avionics experience on SOAR, Jesse has developed flight and safety-critical software, excelling in embedded systems, communication, and GUI programming, ensuring performance and reliability.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="opacity-50" />
+		</div>
+        <div class="card bg-initial card-hover overflow-hidden">
+            <div class="mt-4 flex justify-center items-center">
+                <img src={Lukas} alt="Jesse" class="h-64 object-cover object-contain">
+            </div>
+			<header>
+				<h3 class="h3 mt-4 flex justify-center items-center" data-toc-ignore><strong>Lukas Kobler</strong></h3>
+                <h4 class="h4 flex justify-center items-center text-primary-500" data-toc-ignore>Project Manager</h4>
+			</header>
+			<div class="p-4 space-y-4">
+				<article>
+					<p class="text-center">
+						<!-- cspell:disable -->
+						Lukas is studying mechanical engineering at the University of Calgary and has been developing rocket propulsion systems for SOAR for over 5 years and led the development of Dracarys I, the most powerful hybrid rocket motor ever flown, in Alberta.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="opacity-50" />
+		</div>
+        <div class="card bg-initial card-hover overflow-hidden">
+            <div class="mt-4 flex justify-center items-center">
+                <img src={Wade} alt="Jesse" class="h-64 object-cover object-contain">
+            </div>
+			<header>
+				<h3 class="h3 mt-4 flex justify-center items-center" data-toc-ignore><strong>Xuhui (Wade) Liu                </strong></h3>
+                <h4 class="h4 flex justify-center items-center text-primary-500" data-toc-ignore>Simulation Lead</h4>
+			</header>
+			<div class="p-4 space-y-4">
+				<article>
+					<p class="text-center">
+						<!-- cspell:disable -->
+						Wade has a background in aircraft mechanics and research experience in the AERO-CORE Lab at the University of Calgary. A 12-month internship focused on aircraft structural design and airworthiness certification, provided him with a solid understanding of aerospace engineering.
+						<!-- cspell:enable -->
+					</p>
+				</article>
+			</div>
+			<hr class="opacity-50" />
+		</div>
+    </div>
 </div>
