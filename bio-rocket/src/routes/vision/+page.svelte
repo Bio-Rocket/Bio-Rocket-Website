7affdaea5a878a8d540278ae1f56400d2f255709
Jesse Gerbrandt <jgerbrandt@live.com>
2024-08-10 13:08:25 -0600

    adding vision page

--- bio-rocket/src/routes/vision/+page.svelte
+++ bio-rocket/src/routes/vision/+page.svelte
@@ -1,3 +1,29 @@
-<div class="absolute top-40 left-1/2 transform -translate-x-1/2 font-bold text-4xl">
-    *Coming Soon*
-</div>
+<script>
+    import Vision from '$lib/assets/vision.svelte';
+</script>
+
+<main>
+    <div class="vision">
+        <svelte:component this={Vision} />
+    </div>
+</main>
+
+<style>
+    :global(body, html) {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+    }
+    main {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .vision {
+        width: 100%;
+        display: flex;
+    }
+
+</style>
