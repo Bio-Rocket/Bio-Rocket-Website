8a9cc7a083257648c33daf618526a8b40523b1fc
Jayden Sorensen <jayden.sorensen@outlook.com>
2026-06-10 15:02:35 -0600

    website rebrand, point to new WCAN website

--- bio-rocket/src/routes/about-us/test.cpp
+++
@@ -1,7 +0,0 @@
-BaseType_t xTaskCreate(
-    const char * const pcName, 
-    configSTACK_DEPTH_TYPE usStackDepth,  
-    void *pvParameters,  
-    UBaseType_t uxPriority,  
-    TaskHandle_t *pxCreatedTask  
-);
--- bio-rocket/tailwind.config.ts
+++ bio-rocket/tailwind.config.ts
@@ -1,31 +1,66 @@
-import { join } from 'path'
+// import { join } from 'path'
+// import type { Config } from 'tailwindcss'
+// import forms from '@tailwindcss/forms';
+// import typography from '@tailwindcss/typography';
+// import { skeleton } from '@skeletonlabs/tw-plugin'
+// import { myCustomTheme } from './my-custom-theme'
+
+// export default {
+// 	darkMode: 'class',
+// 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	plugins: [
+// 		forms,
+// 		typography,
+// 		skeleton({
+// 			themes: {
+// 				preset: [
+// 					{
+// 						name: 'skeleton',
+// 						enhancements: true,
+// 					},
+// 				],
+// 				custom: [
+// 					myCustomTheme
+// 				]
+// 			},
+// 		}),
+// 	],
+// } satisfies Config;
+
 import type { Config } from 'tailwindcss'
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
 import { myCustomTheme } from './my-custom-theme'
 
+// const { skeleton } = require('@skeletonlabs/tw-plugin');
+
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'skeleton',
-						enhancements: true,
-					},
-				],
-				custom: [
-					myCustomTheme
-				]
-			},
-		}),
-	],
+    darkMode: 'class',
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        './node_modules/@skeletonlabs/skeleton/**/*.{html,js,svelte,ts}'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        forms,
+        typography,
+        // skeleton({
+        //     themes: {
+        //         preset: [
+        //             {
+        //                 name: 'skeleton',
+        //                 enhancements: true,
+        //             },
+        //         ],
+        //         custom: [
+        //             myCustomTheme
+        //         ]
+        //     },
+        // }),
+    ],
 } satisfies Config;
